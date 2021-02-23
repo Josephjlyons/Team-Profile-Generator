0ec3fd87a878d55ae49052fdd3144b9f356f659c
Joe Lyons <josephjlyons90@gmail.com>
2021-02-23 17:24:03 -0500

    menu inquirer prompt add

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const buildTeamMenu = () => {
+    return inquirer.prompt([
+
+        {
+            type: 'list',
+            name: 'teamAdd',
+            message: 'What position will we be adding to the team today?',
+            choices: [
+                'Engineer',
+                'Intern',
+                "All positions are filled, no need to add anymore"
+
+            ]
+
+        }
+    ])
+}
+buildTeamMenu(); // make sure it works... does... neat!
