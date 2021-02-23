f18d88a3fccbc5eb112691f185146e402529aea2
Joe Lyons <josephjlyons90@gmail.com>
2021-02-23 16:49:46 -0500

    testing of test

---
+++ __test__/Employee.test.js
@@ -0,0 +1,16 @@
+const { TestScheduler } = require("jest");
+const Employee = require('../lib/Employee');
+
+test('can set an employee name', () => {
+    const name  = 'Joe';
+    const employee = new Employee(name);
+
+    expect(employee.name).toBe('Joe'); // compares name 
+});
+
+test('can set employee email', () => {
+    const email = 'josephjlyons90@gmail.com';
+    const employee = new Employee('Joe', 1 , email);
+    
+    expect(employee.getEmail()).toBe('josephjlyons90@gmail.com');
+})
