0dc7a111506a937f787c443cb1996c15ee9f5b24
Joe Lyons <josephjlyons90@gmail.com>
2021-02-25 20:22:12 -0500

    add validations to first prompts

--- index.js
+++ index.js
@@ -1,161 +1,191 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee'); // might not need this? 
+// const Employee = require('./lib/Employee'); // might not need this? 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const teamEmployed = {
-    managers: [],
-    interns: [],
-    engineers: []
+  managers: [],
+  interns: [],
+  engineers: []
 }
 
 
 const buildTeamMenu = () => {
-    inquirer.prompt([
+  inquirer.prompt([
 
-        {
-            type: 'list',
-            name: 'teamAdd',
-            message: 'What position will we be adding to the team today?',
-            choices: [
-                'Engineer',
-                'Intern',
-                'All positions are filled, no need to add anymore'
-            ]
+    {
+      type: 'list',
+      name: 'teamAdd',
+      message: 'What position will we be adding to the team today?',
+      choices: [
+        'Engineer',
+        'Intern',
+        'All positions are filled, no need to add anymore'
+      ]
 
-        }
-    ])
-        .then((answers) => {
-            switch (answers.teamAdd) {
-                case 'Manager':
-                    break;
-                case 'Engineer':
-                    promptEngineer();
-                    break;
-                case 'Intern':
-                    promptIntern();
-                    break;
-                case 'All positions are filled, no need to add anymore': // no break needed at the end of switch/case statement 
-                    buildHTML();
-                    
+    }
+  ])
+    .then((answers) => {
+      switch (answers.teamAdd) {
+        case 'Manager':
+          break;
+        case 'Engineer':
+          promptEngineer();
+          break;
+        case 'Intern':
+          promptIntern();
+          break;
+        case 'All positions are filled, no need to add anymore': // no break needed at the end of switch/case statement 
+          buildHTML();
 
-            }
-        });
+
+      }
+    });
 }
 // Manager questions first before employee prompts 
 
 
 inquirer.prompt([
-    {
-        type: 'input',
-        name: 'managerName',
-        message: 'What is the Manager name?'
-    },
-    {
-        type: 'input',
-        name: 'managerId',
-        message: 'What is the Manager Id'
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'What is the Manager email?'
-    },
-    {
-        type: 'input',
-        name: 'managerOfficeNum',
-        message: 'What is the Managers office number?'
+  {
+    type: 'input',
+    name: 'managerName',
+    message: 'What is the Manager name?',
+    validate: (input) => {
+      if (input.length > 2)
+        return true;
+
+      return "Please enter a valid name!"
+
     }
+
+  },
+  {
+    type: 'number',
+    name: 'managerId',
+    message: 'What is the Manager Id?',
+    validate: (number) => {
+      if (isNaN(number))
+        return "Please enter numerical id!"
+
+      return true
+    }
+  },
+  {
+    type: 'input',
+    name: 'managerEmail',
+    message: 'What is the Manager email?',
+    validate: (input) => {
+      
+        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input)) {
+          return (true)
+        }
+        return "You have entered an invalid email address!"
+        
+      
+
+    }
+  },
+  {
+    type: 'input',
+    name: 'managerOfficeNumber',
+    message: 'What is the Managers office number?',
+    validate: (input) => {
+      if (isNaN(input))
+        return "Please enter office number!"
+
+      return true
+    }
+  }
 ])
-    .then((answers) => {
-        const manager = new Manager(
-            `${answers.managerName}`,
-            `${answers.managerId}`,
-            `${answers.managerEmail}`,
-            `${answers.managerOfficeNumber}`,
-        );
-        teamEmployed.managers.push(manager);
-        buildTeamMenu()
-    });
+  .then((answers) => {
+    const manager = new Manager(
+      `${answers.managerName}`,
+      `${answers.managerId}`,
+      `${answers.managerEmail}`,
+      `${answers.managerOfficeNumber}`,
+    );
+    teamEmployed.managers.push(manager);
+    buildTeamMenu()
+  });
 
 
 
 // Building questions for each role being added
 
 promptEngineer = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'engineerName',
-            message: 'What is the Engineers name?'
-        },
-        {
-            type: 'input',
-            name: 'engineerId',
-            message: 'What is the Engineers Id'
-        },
-        {
-            type: 'input',
-            name: 'engineerEmail',
-            message: 'What is the Engineers email?'
-        },
-        {
-            type: 'input',
-            name: 'engineerGithub',
-            message: 'What is the Engineers Github user name?'
-        }
-    ])
-        .then((answers) => {
-            const engineer = new Engineer(
-                `${answers.engineerName}`,
-                `${answers.engineerId}`,
-                `${answers.engineerEmail}`,
-                `${answers.engineerGithub}`,
-            );
-            teamEmployed.engineers.push(engineer)
-            buildTeamMenu()
-        });
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'engineerName',
+      message: 'What is the Engineers name?'
+    },
+    {
+      type: 'input',
+      name: 'engineerId',
+      message: 'What is the Engineers Id?'
+    },
+    {
+      type: 'input',
+      name: 'engineerEmail',
+      message: 'What is the Engineers email?'
+    },
+    {
+      type: 'input',
+      name: 'engineerGithub',
+      message: 'What is the Engineers Github user name?'
+    }
+  ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        `${answers.engineerName}`,
+        `${answers.engineerId}`,
+        `${answers.engineerEmail}`,
+        `${answers.engineerGithub}`,
+      );
+      teamEmployed.engineers.push(engineer)
+      buildTeamMenu()
+    });
 }
 
 promptIntern = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'internName',
-            message: 'What is the Interns name?'
-        },
-        {
-            type: 'input',
-            name: 'internId',
-            message: 'What is the Interns Id'
-        },
-        {
-            type: 'input',
-            name: 'internEmail',
-            message: 'What is the Interns email?'
-        },
-        {
-            type: 'input',
-            name: 'internSchool',
-            message: 'What is the Interns Github user name?'
-        }
-    ])
-        .then((answers) => {
-            const intern = new Intern(
-                `${answers.internName}`,
-                `${answers.internId}`,
-                `${answers.internEmail}`,
-                `${answers.internSchool}`,
-            );
-            teamEmployed.interns.push(intern);
-            buildTeamMenu()
-        });
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'internName',
+      message: 'What is the Interns name?'
+    },
+    {
+      type: 'input',
+      name: 'internId',
+      message: 'What is the Interns Id?'
+    },
+    {
+      type: 'input',
+      name: 'internEmail',
+      message: 'What is the Interns email?'
+    },
+    {
+      type: 'input',
+      name: 'internSchool',
+      message: 'Where did the Intern attend school?'
+    }
+  ])
+    .then((answers) => {
+      const intern = new Intern(
+        `${answers.internName}`,
+        `${answers.internId}`,
+        `${answers.internEmail}`,
+        `${answers.internSchool}`,
+      );
+      teamEmployed.interns.push(intern);
+      buildTeamMenu()
+    });
 }
 
 const buildHTMLPage = () =>
-    // INSERT HTML FILE HERE 
-    `<!DOCTYPE html>
+  // INSERT HTML FILE HERE 
+  `<!DOCTYPE html>
   <html lang="en">
   
   <head>
@@ -164,14 +194,15 @@ const buildHTMLPage = () =>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
-    <title>Employee Cards</title>
+    <link rel="stylesheet" href="../src/style.css">
+    <title>The Team</title>
   </head>
   
-  <body>
-    <section class="hero is-small is-primary mb-3">
+  <body class="body">
+    <section class="hero is-small is-primary mb-3 has-background-black">
       <div class="hero-body">
         <h1 class="title has-text-centered">
-        The Squad
+        The Team
         </h1>
       </div>
     </section>
@@ -208,22 +239,22 @@ const buildHTMLPage = () =>
 
 let buildHTML = () => {
 
-    fs.writeFile('./generated-HTML/main.html', buildHTMLPage(), (err) => {
-        err ? console.log(err, "Something went wrong :(") : console.log('Team created - check generate-HTML folder to view')
-    })
+  fs.writeFile('./generated-HTML/main.html', buildHTMLPage(), (err) => {
+    err ? console.log(err, "Something went wrong :(") : console.log('Team created - check generate-HTML folder to view')
+  })
 }
 
 // Building Manager Card(s)
 
 function createManagerCard(manager) {
-    return `<div class="card is-one-third has-text-centered ">
+  return `<div class="card is-one-third has-text-centered">
    
   <div class="card-content">
     <div class="media">
       <div class="media-left">
       </div>
       <div class="media-content">
-        <p class="title is-4"> Manager <i class="fas fa-briefcase"></i></p>
+        <p class="title is-4"> Manager <i class="fas fa-sitemap"></i></p>
         <p class="subtitle is-6">${manager.name}</p>
       </div>
     </div>
@@ -231,24 +262,24 @@ function createManagerCard(manager) {
     <div class="content">
       <p>Employee ID: ${manager.id}</p>
       <a target="_blank" href="mailto:${manager.email}">${manager.email}</a>
-      <p>Office Number: ${manager.officeNumber}</p>
+      <p>Office Number: ${manager.officeNumber.slice(0,3) + "-" + manager.officeNumber.slice(3,6) + "-" + manager.officeNumber.slice(6)}</p>
     </div>
   </div>
   </div>`
 
 }
 function buildManagerCollection() {
-    let managerCollection = "";
-    teamEmployed.managers.forEach(manager => {
-        managerCollection += createManagerCard(manager);
-    })
-    return managerCollection
-} 
+  let managerCollection = "";
+  teamEmployed.managers.forEach(manager => {
+    managerCollection += createManagerCard(manager);
+  })
+  return managerCollection
+}
 
 // Build Engineer Card(s)
 
 function buildEngineerCard(engineer) {
-    return `<div class="card is-one-third has-text-centered ">
+  return `<div class="card is-one-third has-text-centered ">
    
   <div class="card-content">
     <div class="media">
@@ -270,17 +301,17 @@ function buildEngineerCard(engineer) {
 
 }
 function buildEngineerCollection() {
-    let engineerCollection = "";
-    teamEmployed.engineers.forEach(engineer => {
-        engineerCollection += buildEngineerCard(engineer);
-    })
-    return engineerCollection
-} 
+  let engineerCollection = "";
+  teamEmployed.engineers.forEach(engineer => {
+    engineerCollection += buildEngineerCard(engineer);
+  })
+  return engineerCollection
+}
 
 // Build Intern Card(s)
 
 function buildInternCard(intern) {
-    return `<div class="card is-one-third has-text-centered ">
+  return `<div class="card is-one-third has-text-centered ">
    
   <div class="card-content">
     <div class="media">
@@ -302,9 +333,9 @@ function buildInternCard(intern) {
 
 }
 function buildInternCollection() {
-    let internCollection = "";
-    teamEmployed.interns.forEach(Intern => {
-        internCollection += buildInternCard(Intern);
-    })
-    return internCollection
+  let internCollection = "";
+  teamEmployed.interns.forEach(Intern => {
+    internCollection += buildInternCard(Intern);
+  })
+  return internCollection
 } 
